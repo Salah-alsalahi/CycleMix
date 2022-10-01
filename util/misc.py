8bece4580734f91190b94d52276bb6fb4d32175b
Salah-alsalahi <salahalsalahi.king@gmail.com>
2022-10-01 01:07:38 +0000

    fd

--- util/misc.py
+++ util/misc.py
@@ -12,9 +12,9 @@ from torch import Tensor
 
 # needed due to empty tensor bug in pytorch and torchvision 0.5
 import torchvision
-if float(torchvision.__version__[:3]) < 0.7:
-    from torchvision.ops import _new_empty_tensor
-    from torchvision.ops.misc import _output_size
+# if float(torchvision.__version__[:3]) < 0.7:
+#     from torchvision.ops import _new_empty_tensor
+#     from torchvision.ops.misc import _output_size
 
 
 class SmoothedValue(object):
@@ -433,8 +433,12 @@ def interpolate(input, size=None, scale_factor=None, mode="nearest", align_corne
     Equivalent to nn.functional.interpolate, but with support for empty batch sizes.
     This will eventually be supported natively by PyTorch, and this
     class can go away.
+
+git config --global user.email "salahalsalahi.king@gmail.com"
+git config --global user.name "Salah-alsalahi"
+
     """
-    if float(torchvision.__version__[:3]) < 0.7:
+    if float(torchvision.__version__[:3]) > 0.7:
         if input.numel() > 0:
             return torch.nn.functional.interpolate(
                 input, size, scale_factor, mode, align_corners
